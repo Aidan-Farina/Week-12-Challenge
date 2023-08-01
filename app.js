1154948b0df5ec431840401e34fb1ddaa5be8d7e
Aidan <farinaaidan2@gmail.com>
2023-08-01 17:32:08 -0400

    worked on the view tables answers

--- app.js
+++ app.js
@@ -17,4 +17,47 @@ function start() {
             "Exit",
         ],
     })
+        .then(handleAction);
 }
+function handleAction(answer) {
+    switch (answer.action) {
+        case "View all departments":
+            viewAll("department");
+            break;
+        case "View all roles":
+            viewAll("role");
+            break;
+        case "View all employees":
+            viewAll("employee");
+            break;
+        case "Add a department":
+            addDepartment();
+            break;
+        case "Add a role":
+            addRole();
+            break;
+        case "Add an employee":
+            addEmployee();
+            break;
+        case "Update an employee role":
+            updateRole();
+            break;
+        case "Exit":
+            connection.end();
+            break;
+        default:
+            console.log(`Invalid action: ${answer.action}`);
+            break;
+    }
+}
+
+
+function viewAll(tableName) {
+    const query = `SELECT * FROM ${tableName}`;
+    connection.query(query, (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      start();
+    });
+  }
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,19 @@
 INSERT INTO department (department_name)
 VALUES ("Management"),
        ("Human Resources"),
-       ("Customer Service"),
+       ("Customer Service");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("CEO", 1000000, 1),
        ("Human Resource Manager", 80000, 2),
-       ("Human Resource Person", 50000, 2)
+       ("Human Resource Person", 50000, 2),
        ("Customer Service Manager", 70000, 3),
-       ("Customer Service Person", 40000, 3)
+       ("Customer Service Person", 40000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ("Michael", "Prevost", 1, NULL)
-        ("Jenn", "Butler", 2, 1)
-        ("Kat", "Gorecki", 3, 2)
-        ("Austin", "Waller", 4, 1)
-        ("Gabriel", "Cruz", 5, 4)
+VALUES  ("Michael", "Prevost", 1, NULL),
+        ("Jenn", "Butler", 2, 1),
+        ("Kat", "Gorecki", 3, 2),
+        ("Austin", "Waller", 4, 1),
+        ("Gabriel", "Cruz", 5, 4);
        
